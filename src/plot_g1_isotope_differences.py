d126ecd330d276ba836ba5d37b21c24dc652dddb
mdekauwe <mdekauwe@gmail.com>
2019-04-23 13:03:10 +1000

    Added hydroclimate

--- src/plot_g1_isotope_differences.py
+++ src/plot_g1_isotope_differences.py
@@ -20,6 +20,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.stats
+from sklearn.cluster import KMeans
 
 def stars(p):
    if p < 0.0001:
@@ -31,7 +32,7 @@ def stars(p):
    elif (p < 0.05):
        return "*"
    else:
-       return "-"
+       return "ns"
 
 def compare_within_Australia(df):
 
@@ -168,17 +169,190 @@ def compare_within_ebt(df):
                 bbox_inches='tight', pad_inches=0.1)
 
 def compare_within_hydroclimate(df):
+    """
+    Use K-Mean clustering to figure out the unique classes to compare across
+    """
 
     # Cut off g1 at 14 for visual purposes
     #df = df[df.g1 <= 14]
 
     df_euc = df[df['orig_spp'].str.contains('Eucalyptus')]
+
+    # extracted from
+    # http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip
+    # https://gist.github.com/graydon/11198540
+    df_euc = df_euc[(df_euc.latitude >= -43.6345972634) &
+                    (df_euc.latitude <= -10.6681857235) &
+                    (df_euc.longitude >= 113.338953078) &
+                    (df_euc.longitude <= 153.569469029)]
+
     df_other = df[~ df['orig_spp'].str.contains('Eucalyptus')]
     df_other = df_other[(df_other['PFT'] == "EBF") | (df_other['PFT'] == "TRF")]
 
+    df1 = df_euc[['map','g1']]
+    data = df1.as_matrix()
+
+    # create kmeans object
+    kmeans = KMeans(n_clusters=4)
+
+    # fit kmeans object to data
+    kmeans.fit(data)
+
+    # print location of clusters learned by kmeans object
+    #print(kmeans.cluster_centers_)
+
+    y_km = kmeans.fit_predict(data)
+
+    #plt.scatter(data[y_km ==0,0], data[y_km == 0,1], s=100, c='red')
+    #plt.scatter(data[y_km ==1,0], data[y_km == 1,1], s=100, c='black')
+    #plt.scatter(data[y_km ==2,0], data[y_km == 2,1], s=100, c='blue')
+    #plt.scatter(data[y_km ==3,0], data[y_km == 3,1], s=100, c='cyan')
+    #plt.show()
+
+    clus_1_low, clus_1_hig = np.min(data[y_km ==0,0]), np.max(data[y_km ==0,0])
+    clus_2_low, clus_2_hig = np.min(data[y_km ==1,0]), np.max(data[y_km ==1,0])
+    clus_3_low, clus_3_hig = np.min(data[y_km ==2,0]), np.max(data[y_km ==2,0])
+    clus_4_low, clus_4_hig = np.min(data[y_km ==3,0]), np.max(data[y_km ==3,0])
+
+    # Clusters aren't sorted, fix this
+    (clus_1_low, clus_2_low,
+     clus_3_low, clus_4_low) = sorted( (clus_1_low, clus_2_low, clus_3_low,
+                                        clus_4_low))
+    # Clusters aren't sorted, fix this
+    (clus_1_hig, clus_2_hig,
+     clus_3_hig, clus_4_hig) = sorted( (clus_1_hig, clus_2_hig, clus_3_hig,
+                                        clus_4_hig))
+
+    print(clus_1_low, clus_2_low,clus_3_low, clus_4_low)
+    print(clus_1_hig, clus_2_hig,clus_3_hig, clus_4_hig)
+
+    euc_clus_1 = df_euc[(df_euc.map >= clus_1_low) & \
+                        (df_euc.map < clus_1_hig)].g1.values
+    euc_clus_2 = df_euc[(df_euc.map >= clus_2_low) & \
+                        (df_euc.map < clus_2_hig)].g1.values
+    euc_clus_3 = df_euc[(df_euc.map >= clus_3_low) & \
+                        (df_euc.map < clus_3_hig)].g1.values
+    euc_clus_4 = df_euc[(df_euc.map >= clus_4_low) & \
+                        (df_euc.map < clus_4_hig)].g1.values
+
+    other_clus_1 = df_other[(df_other.map >= clus_1_low) & \
+                            (df_other.map < clus_1_hig)].g1.values
+    other_clus_2 = df_other[(df_other.map >= clus_2_low) & \
+                            (df_other.map < clus_2_hig)].g1.values
+    other_clus_3 = df_other[(df_other.map >= clus_3_low) & \
+                            (df_other.map < clus_3_hig)].g1.values
+    other_clus_4 = df_other[(df_other.map >= clus_4_low) & \
+                            (df_other.map < clus_4_hig)].g1.values
+
+    data_a = [euc_clus_1, euc_clus_2, euc_clus_3, euc_clus_4]
+    data_b = [other_clus_1, other_clus_2, other_clus_3, other_clus_4]
+
+    fig = plt.figure(figsize=(9,6))
+    fig.subplots_adjust(hspace=0.1)
+    fig.subplots_adjust(wspace=0.05)
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['font.family'] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = "Helvetica"
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['legend.fontsize'] = 14
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+
+    ax = fig.add_subplot(111)
+
+    ticks = ['%d-%d mm' % (round(clus_1_low,1),round(clus_1_hig,1)),\
+             '%d-%d mm' % (round(clus_2_low,1),round(clus_2_hig,1)),\
+             '%d-%d mm' % (round(clus_3_low,1),round(clus_3_hig,1)),\
+             '%d-%d mm' % (round(clus_4_low,1),round(clus_4_hig,1))]
+
+
+    xloc_a = np.array(range(len(data_a)))*2.0-0.4
+    xloc_b = np.array(range(len(data_b)))*2.0+0.4
+
+
+    bpl = ax.boxplot(data_a, positions=xloc_a, sym='', widths=0.6)
+    bpr = ax.boxplot(data_b, positions=xloc_b, sym='', widths=0.6)
+    set_box_color(bpl, '#D7191C') # colors are from http://colorbrewer2.org/
+    set_box_color(bpr, '#2C7BB6')
+
+
+    # draw temporary red and blue lines and use them to create a legend
+    ax.plot([], c='#D7191C', label='Eucalyptus species')
+    ax.plot([], c='#2C7BB6', label='EBF')
+    plt.xticks(range(0, len(ticks) * 2, 2), ticks)
+    ax.set_xlim(-2, len(ticks)*2)
+    ax.set_ylim(0, 9)
+    ax.legend(numpoints=1, loc="upper left")
+    ax.set_ylabel("$g_1$ (kPa$^{0.5}$)")
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    # non-parametric alternative to the t-test, Mann-Whitney U-test
+    z, p = scipy.stats.mannwhitneyu(data_a[0], data_b[0])
+    p_value = p * 2 # two tailed
+
+    y_max = 6.5
+    y_min = 0
+
+    ax.annotate("", xy=(xloc_a[0], y_max), xycoords='data',
+                xytext=(xloc_b[0], y_max), textcoords='data',
+                arrowprops=dict(arrowstyle="-", ec='#aaaaaa',
+                connectionstyle="bar,fraction=0.2"))
+    ax.text(xloc_a[0]+0.4, y_max+0.5, stars(p_value),
+            horizontalalignment='center',
+            verticalalignment='center')
+
+    # non-parametric alternative to the t-test, Mann-Whitney U-test
+    z, p = scipy.stats.mannwhitneyu(data_a[1], data_b[1])
+    p_value = p * 2 # two tailed
+
+    y_max = 6.5
+    y_min = 0
+
+    ax.annotate("", xy=(xloc_a[1], y_max), xycoords='data',
+                xytext=(xloc_b[1], y_max), textcoords='data',
+                arrowprops=dict(arrowstyle="-", ec='#aaaaaa',
+                connectionstyle="bar,fraction=0.2"))
+    ax.text(xloc_a[1]+0.4, y_max+0.5, stars(p_value),
+            horizontalalignment='center',
+            verticalalignment='center')
+
+    # non-parametric alternative to the t-test, Mann-Whitney U-test
+    z, p = scipy.stats.mannwhitneyu(data_a[2], data_b[2])
+    p_value = p * 2 # two tailed
+
+    y_max = 6.5
+    y_min = 0
+
+    ax.annotate("", xy=(xloc_a[2], y_max), xycoords='data',
+                xytext=(xloc_b[2], y_max), textcoords='data',
+                arrowprops=dict(arrowstyle="-", ec='#aaaaaa',
+                connectionstyle="bar,fraction=0.2"))
+    ax.text(xloc_a[2]+0.4, y_max+0.5, stars(p_value),
+            horizontalalignment='center',
+            verticalalignment='center')
+
+    # non-parametric alternative to the t-test, Mann-Whitney U-test
+    z, p = scipy.stats.mannwhitneyu(data_a[3], data_b[3])
+    p_value = p * 2 # two tailed
+
+    y_max = 6.5
+    y_min = 0
+
+    ax.annotate("", xy=(xloc_a[3], y_max), xycoords='data',
+                xytext=(xloc_b[3], y_max), textcoords='data',
+                arrowprops=dict(arrowstyle="-", ec='#aaaaaa',
+                connectionstyle="bar,fraction=0.2"))
+    ax.text(xloc_a[3]+0.4, y_max+0.5, stars(p_value),
+            horizontalalignment='center',
+            verticalalignment='center')
+
+    odir = "plots"
+    fig.savefig(os.path.join(odir, "g1_isotope_boxplot_hydroclimate.pdf"),
+                bbox_inches='tight', pad_inches=0.1)
+
     sys.exit()
-    data = [df_euc.map.values, df_other.map.values]
-    print( round(np.median(data[0]), 3), round(np.median(data[1]),3) )
 
     # non-parametric alternative to the t-test, Mann-Whitney U-test
     z, p = scipy.stats.mannwhitneyu(data[0], data[1])
@@ -230,11 +404,17 @@ def compare_within_hydroclimate(df):
                 bbox_inches='tight', pad_inches=0.1)
 
 
+def set_box_color(bp, color):
+    plt.setp(bp['boxes'], color=color)
+    plt.setp(bp['whiskers'], color=color)
+    plt.setp(bp['caps'], color=color)
+    plt.setp(bp['medians'], color=color)
+
 if __name__ == "__main__":
 
     fdir = "data/g1/processed"
     df = pd.read_csv(os.path.join(fdir, "g1_isotope_screened_mapai.csv"))
 
-    compare_within_Australia(df)
-    compare_within_ebt(df)
-    #compare_within_hydroclimate(df)
+    #compare_within_Australia(df)
+    #compare_within_ebt(df)
+    compare_within_hydroclimate(df)
