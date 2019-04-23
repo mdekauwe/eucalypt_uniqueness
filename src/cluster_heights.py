2c7e1e5fe1fb5e17100b4b756703478a93cebbce
mdekauwe <mdekauwe@gmail.com>
2019-04-24 09:35:55 +1000

    Fixed height plot

--- src/cluster_heights.py
+++ src/cluster_heights.py
@@ -23,48 +23,63 @@ import scipy.stats
 from sklearn.cluster import KMeans
 import gdal
 
-def cluster_eucs():
+def cluster_heights(df):
 
-    """
+    data = df.height.values.reshape(-1,1) # need 2d array
+
     # create kmeans object
     MYSEED = 5
-    kmeans = KMeans(n_clusters=4, random_state=MYSEED)
+    kmeans = KMeans(n_clusters=5, random_state=MYSEED)
+
 
     # fit kmeans object to data
     kmeans.fit(data)
 
     # print location of clusters learned by kmeans object
-    #print(kmeans.cluster_centers_)
+    clusters = sorted(np.array([i[0] for i in kmeans.cluster_centers_]))
+    print(clusters)
 
-    y_km = kmeans.fit_predict(data)
-
-    #plt.scatter(data[y_km ==0,0], data[y_km == 0,1], s=100, c='red')
-    #plt.scatter(data[y_km ==1,0], data[y_km == 1,1], s=100, c='black')
-    #plt.scatter(data[y_km ==2,0], data[y_km == 2,1], s=100, c='blue')
-    #plt.scatter(data[y_km ==3,0], data[y_km == 3,1], s=100, c='cyan')
-    #plt.show()
-
-    clus_1_low, clus_1_hig = np.min(data[y_km ==0,0]), np.max(data[y_km ==0,0])
-    clus_2_low, clus_2_hig = np.min(data[y_km ==1,0]), np.max(data[y_km ==1,0])
-    clus_3_low, clus_3_hig = np.min(data[y_km ==2,0]), np.max(data[y_km ==2,0])
-    clus_4_low, clus_4_hig = np.min(data[y_km ==3,0]), np.max(data[y_km ==3,0])
-
-    # Clusters aren't sorted, fix this
-    (clus_1_low, clus_2_low,
-     clus_3_low, clus_4_low) = sorted( (clus_1_low, clus_2_low, clus_3_low,
-                                        clus_4_low))
-    # Clusters aren't sorted, fix this
-    (clus_1_hig, clus_2_hig,
-     clus_3_hig, clus_4_hig) = sorted( (clus_1_hig, clus_2_hig, clus_3_hig,
-                                        clus_4_hig))
-
-    print(clus_1_low, clus_2_low,clus_3_low, clus_4_low)
-    print(clus_1_hig, clus_2_hig,clus_3_hig, clus_4_hig)
-    """
-
+    return (clusters)
 
 if __name__ == "__main__":
 
     fdir = "data/height/"
     fn = "euc_heights.csv"
     df = pd.read_csv(os.path.join(fdir, fn))
+
+    df_euc = df[df['species'].str.contains('Eucalyptus')]
+    df_other = df[~ df['species'].str.contains('Eucalyptus')]
+
+    euc_clust = cluster_heights(df_euc)
+    other_clust = cluster_heights(df_other)
+
+    fig = plt.figure(figsize=(9,6))
+    fig.subplots_adjust(hspace=0.1)
+    fig.subplots_adjust(wspace=0.05)
+    plt.rcParams['text.usetex'] = False
+    plt.rcParams['font.family'] = "sans-serif"
+    plt.rcParams['font.sans-serif'] = "Helvetica"
+    plt.rcParams['axes.labelsize'] = 14
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['legend.fontsize'] = 14
+    plt.rcParams['xtick.labelsize'] = 14
+    plt.rcParams['ytick.labelsize'] = 14
+
+    ax = fig.add_subplot(111)
+
+    #ax.hist(df_euc.height.values, bins=20, density=True,
+    #        ls='-', lw=1, facecolor="royalblue", edgecolor="lightgrey")
+
+    sns.distplot(df_euc.height.values, ax=ax, rug=False, norm_hist=True,
+                 kde_kws={"label": "KDE"})
+
+    for i in euc_clust:
+        ax.axvline(x=i, ls="--", color="grey")
+
+    ax.set_xlabel('Height (m)')
+    ax.set_ylabel('Probability density')
+    plt.show()
+
+    odir = "plots"
+    fig.savefig(os.path.join(odir, "Euc_height_histogram.pdf"),
+                bbox_inches='tight', pad_inches=0.1)
